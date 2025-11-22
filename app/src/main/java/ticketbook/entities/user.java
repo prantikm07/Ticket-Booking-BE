0c3a8276c92a0a0d9d996085a71d06d3a215eef4
prantikm07 <prantik25m@gmail.com>
2025-11-23 01:04:44 +0530

    Structure Created by Gradle

---
+++ app/src/main/java/ticketbook/entities/user.java
@@ -0,0 +1,10 @@
+package ticketbook.entities;
+import java.util.*;
+
+public class user {
+    private String name;
+    private String userId;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBooked;
+}
